a1f3d72e64963d9f130c68677d0e43730ca6424b
atakishiyevakhadija <atakishiyevakhadija@gmail.com>
2026-02-04 19:12:46 +0400

    Pagination and search

--- library/src/main/java/com/library/dea/controller/BookPageController.java
+++ library/src/main/java/com/library/dea/controller/BookPageController.java
@@ -3,6 +3,7 @@ package com.library.dea.controller;
 import com.library.dea.entity.Book;
 import com.library.dea.service.BookService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,8 +21,14 @@ public class BookPageController {
 
      //table
     @GetMapping
-    public String showBooks(Model model){
-        model.addAttribute("books", bookService.showAll());
+    public String showBooks(  @RequestParam(defaultValue = "0")int page,
+                              @RequestParam(defaultValue = "5")int size,
+                              Model model){
+        Page<Book> bookPage = bookService.findPaginated(page, size);
+        model.addAttribute("books", bookPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", bookPage.getTotalPages());
+        model.addAttribute("size", size);
         return "library/list";
     }
 
@@ -55,5 +62,21 @@ public class BookPageController {
     public String delete(@PathVariable Integer id){
         bookService.deleteBook(id);
         return "redirect:/books";
+    }
+
+    @GetMapping("/search")
+    public String searchBooks(
+            @RequestParam String keyword,
+            @RequestParam(defaultValue = "0")int page,
+            @RequestParam(defaultValue = "5")int size,
+            Model model
+    ){
+        Page<Book> bookPage = bookService.search(keyword, page, size);
+        model.addAttribute("books", bookPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", bookPage.getTotalPages());
+        model.addAttribute("size", size);
+        model.addAttribute("keyword", keyword);
+        return "library/list";
     }
 }
